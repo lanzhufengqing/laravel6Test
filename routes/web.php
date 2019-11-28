c5896f5c7f2dd49fe2e8d5d96cfe37e261b44c41
lanzhufengqing <452716761@qq.com>
2019-11-28 22:24:21 +0800

    用户注册页面

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,9 @@
 
 //微博 路由
 Route::get('/', 'StaticPagesController@home');
-Route::get('/help', 'StaticPagesController@help');
-Route::get('/about', 'StaticPagesController@about');
+Route::get('/help', 'StaticPagesController@help')->name('help');
+Route::get('/about', 'StaticPagesController@about')->name('about');
+Route::get('signup', 'UsersController@create')->name('signup');
 
 
 Route::get('/hello', function () {
