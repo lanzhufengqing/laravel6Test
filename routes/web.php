26cb607117480c247f399ff690153e3f2d4d4b44
lanzhufengqing <452716761@qq.com>
2019-11-29 17:18:04 +0800

    用户注册 session缓存

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@
 
 //微博 路由
 Route::get('/', 'StaticPagesController@home');
-Route::get('/help', 'StaticPagesController@help')->name('help');
+Route::get('/help1', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 Route::get('signup', 'UsersController@create')->name('signup');
 Route::resource('users','UsersController');
