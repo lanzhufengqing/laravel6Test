ab232056e6ec0747ef0a4c25eaa4fef1221fdab0
lanzhufengqing <452716761@qq.com>
2019-12-03 17:57:18 +0800

    微博模型

---
+++ app/Models/Statue.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * 微博模型-动态 用户和微博 一对多关系
+ */
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Statue extends Model
+{
+    /**
+     * 该方法指明一条微博属于一个用户，函数名单数
+     * @return [type] [description]
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -56,7 +56,19 @@ class User extends Authenticatable
         parent::boot();
 
         static::creating(function ($user) {
+            //数据入库前生成给用户生成activation_token注册令牌
             $user->activation_token = Str::random(10);
         });
     }
+
+    /**
+     * 用户和微博一对多关联，指明一个用户可以有多条微博，函数名复数
+     * @return [type] [description]
+     */
+    public function statuses()
+    {
+        return $this->hasMany(Status::class);
+    }
+
+
 }
